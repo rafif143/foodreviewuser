65ffc06a239e2f75edb99d50a4082bdf6e8d55c0
rafif143 <rafifahdan@gmail.com>
2025-08-28 19:30:39 +0700

    ssr

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: false
-		})
-	}
+  kit: {
+    adapter: adapter(),
+  }
 };
 
 export default config;
