01a9429dd62f273186386cbf94489763aa62232b
rafif143 <rafifahdan@gmail.com>
2025-09-15 08:04:13 +0700

    11

--- src/lib/tenant.js
+++ src/lib/tenant.js
@@ -1,7 +1,7 @@
 import { supabase, isSupabaseConfigured } from './supabase';
 
 // Default website ID - hardcode saja
-export const DEFAULT_WEBSITE_ID = 10;
+export const DEFAULT_WEBSITE_ID = 11;
 
 // Cache untuk menyimpan data website
 let websiteCache = new Map();
